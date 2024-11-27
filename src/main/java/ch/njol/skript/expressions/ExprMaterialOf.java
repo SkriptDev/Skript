0a6b1a9b57cb9df235164cce6e48c93258d64a6a
ShaneBeee <shanebolenback@me.com>
2024-11-26 17:27:31 -0800

    ExprMaterialOf - some doc updates, warnings and fixes

--- src/main/java/ch/njol/skript/expressions/ExprMaterialOf.java
+++ src/main/java/ch/njol/skript/expressions/ExprMaterialOf.java
@@ -1,10 +1,13 @@
 package ch.njol.skript.expressions;
 
+import ch.njol.skript.Skript;
 import ch.njol.skript.classes.Changer.ChangeMode;
 import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
 import ch.njol.skript.doc.Name;
 import ch.njol.skript.doc.Since;
 import ch.njol.skript.expressions.base.SimplePropertyExpression;
+import ch.njol.skript.lang.Expression;
 import ch.njol.util.coll.CollectionUtils;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -14,7 +17,14 @@ import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.Nullable;
 
 @Name("Material Of")
-@Description("Get/set the material of an item/block/blockdata.")
+@Description({"Get the material of an ItemStack/Block/BlockData.",
+	"Setting the material of an ItemStack is not supported and may result in weird behaviour.",
+	"Setting the material of a BlockData is not supported.",
+	"Setting the material of a Block will act normally."})
+@Examples({"if material of player's tool = diamond:",
+	"set {_mat} to material of player's tool",
+	"set {_mat} to material of target block of player",
+	"set material of target block of player to oak log"})
 @Since("INSERT VERSION")
 public class ExprMaterialOf extends SimplePropertyExpression<Object, Material> {
 
@@ -37,16 +47,24 @@ public class ExprMaterialOf extends SimplePropertyExpression<Object, Material> {
 
 	@Override
 	public @Nullable Class<?>[] acceptChange(ChangeMode mode) {
+		if (getExpr() != null && getExpr().getReturnType() == ItemStack.class) {
+			Skript.warning("The material of an ItemStack cannot properly be set and may result in issues." +
+				"Instead you should set the ItemStack to a new ItemStack.");
+		}
 		if (mode == ChangeMode.SET) {
 			return CollectionUtils.array(Material.class);
 		}
 		return null;
 	}
 
+	@SuppressWarnings("deprecation") // Warning provided during acceptChange
 	@Override
 	public void change(Event event, @Nullable Object[] delta, ChangeMode mode) {
+		Expression<?> expr = getExpr();
+		if (expr == null) return;
+
 		if (delta != null && delta[0] instanceof Material material) {
-			for (Object object : getExpr().getArray(event)) {
+			for (Object object : expr.getArray(event)) {
 				if (object instanceof ItemStack itemStack) {
 					itemStack.setType(material);
 				} else if (object instanceof Block block) {
@@ -54,7 +72,6 @@ public class ExprMaterialOf extends SimplePropertyExpression<Object, Material> {
 				}
 			}
 		}
-
 	}
 
 	@Override
