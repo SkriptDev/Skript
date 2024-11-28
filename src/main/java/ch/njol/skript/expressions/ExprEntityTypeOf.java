f59ec15b72398b1e0c4c636c3174b0b078a28e7a
ShaneBeee <shanebolenback@me.com>
2024-11-27 16:34:45 -0800

    ExprEntityTypeOf - add missing docs

--- src/main/java/ch/njol/skript/expressions/ExprEntityTypeOf.java
+++ src/main/java/ch/njol/skript/expressions/ExprEntityTypeOf.java
@@ -1,14 +1,23 @@
 package ch.njol.skript.expressions;
 
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Name;
+import ch.njol.skript.doc.Since;
 import ch.njol.skript.expressions.base.SimplePropertyExpression;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.jetbrains.annotations.Nullable;
 
+@Name("Entity Type")
+@Description("Get the EntityType of an entity.")
+@Examples({"set {_type} to entity type of target entity",
+	"if entity type of target = pig:"})
+@Since("INSERT VERSION")
 public class ExprEntityTypeOf extends SimplePropertyExpression<Entity, EntityType> {
 
 	static {
-		register(ExprEntityTypeOf.class, EntityType.class, "entity[ ]type", "entities");
+		register(ExprEntityTypeOf.class, EntityType.class, "entity[ ]type[s]", "entities");
 	}
 
 	@Override
