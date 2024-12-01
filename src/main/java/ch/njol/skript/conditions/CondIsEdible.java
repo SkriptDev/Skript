a39c674512c6008e2a006c9df4b42e2817d6f394
ShaneBeee <shanebolenback@me.com>
2024-12-01 09:51:46 -0800

    CondIsEdible - support ItemStacks and checking for food/consumable components

--- src/main/java/ch/njol/skript/conditions/CondIsEdible.java
+++ src/main/java/ch/njol/skript/conditions/CondIsEdible.java
@@ -1,43 +1,50 @@
-/**
- *   This file is part of Skript.
- *
- *  Skript is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  Skript is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with Skript.  If not, see <http://www.gnu.org/licenses/>.
- *
- * Copyright Peter Güttinger, SkriptLang team and contributors
- */
 package ch.njol.skript.conditions;
 
+import ch.njol.skript.Skript;
 import ch.njol.skript.conditions.base.PropertyCondition;
 import ch.njol.skript.doc.Description;
 import ch.njol.skript.doc.Examples;
+import ch.njol.skript.doc.Keywords;
 import ch.njol.skript.doc.Name;
 import ch.njol.skript.doc.Since;
+import io.papermc.paper.datacomponent.DataComponentTypes;
 import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
 
 @Name("Is Edible")
-@Description("Checks whether a material is edible.")
-@Examples({"steak is edible", "player's tool is edible"}) //todo fix docs
+@Description({"Checks whether a Material/ItemStack is edible/consumable.",
+	"If running MC 1.20.6-1.21.1: Will check if an ItemStack has a food component.",
+	"If running MC 1.21.3+: Will check if an ItemStack has a consumable component."})
+@Examples({"cooked beef is edible", "player's tool is edible"})
 @Since("2.2-dev36")
-public class CondIsEdible extends PropertyCondition<Material> {
+@Keywords("consumable")
+public class CondIsEdible extends PropertyCondition<Object> {
+
+	private static final boolean HAS_CONSUMABLE;
+	private static final boolean HAS_FOOD;
 
 	static {
-		PropertyCondition.register(CondIsEdible.class, "edible", "materials");
+		HAS_CONSUMABLE = Skript.classExists("io.papermc.paper.datacomponent.item.Consumable");
+		boolean food = Skript.classExists("org.bukkit.inventory.meta.components.FoodComponent");
+		HAS_FOOD = food && !HAS_CONSUMABLE;
+
+		PropertyCondition.register(CondIsEdible.class, "(edible|consumable)", "materials/itemstacks");
 	}
 
+	@SuppressWarnings("UnstableApiUsage")
 	@Override
-	public boolean check(Material material) {
-		return material.isEdible();
+	public boolean check(Object object) {
+		if (object instanceof Material material) {
+			return material.isEdible();
+		} else if (object instanceof ItemStack itemstack) {
+			if (HAS_CONSUMABLE && itemstack.hasData(DataComponentTypes.CONSUMABLE)) {
+				return true;
+			} else if (HAS_FOOD && itemstack.getItemMeta().hasFood()) {
+				return true;
+			}
+			return itemstack.getType().isEdible();
+		}
+		return false;
 	}
 
 	@Override
